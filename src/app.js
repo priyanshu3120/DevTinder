35ce1d1b20e709a0a855489065baab5ec215a830
Priya <priyanshumaurya8721@gmail.com>
2025-09-24 02:59:07 +0530

    Handling Multiple Routes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 const express = require("express");
 
 const app = express();
-
-app.get("/user",(req,res)=>{
-    res.send({name:"priyanshu",age:23,course:"btech"});
-})
-
-app.post("/user",(req,res)=>{
-    res.send("your data will be saved in database");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("your data deleted sucessfully");
-})
-
-app.use("/",(req,res) =>{
-    res.send("Welcome to the server");
-})
+// Handling the multiple routes
+app.use("/user",(req,res,next)=>{
+    console.log("print the respond 1st");
+    next();
+    //res.send("1st Respond!!");
+},
+(req,res,next)=>{
+    console.log("print the respond 2nd");
+    //res.send("2nd Respond!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("print the respond 3rd");
+    res.send("3rd Respond!!");
+    //next();
+}
+)
 
 app.listen(5000, ()=>{
     console.log("server will be listen at port 5000...");
