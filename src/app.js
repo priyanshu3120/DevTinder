e65ccb23498e22401670d3a540c4f170c368554c
Priya <priyanshumaurya8721@gmail.com>
2025-09-25 15:52:52 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -3,15 +3,10 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async(req,res)=>{
-    const user = new User({
-        firstName:"Priyanshu",
-        lastName:"Maurya",
-        gender:"male",
-        emailId:"priyanshu@gmail.com",
-        password:"priyanshu@123",
-        age:23,
-    })
+    const user = new User(req.body);
     try{
         await user.save();
         res.send("Data added sucessfully");
