23a0382927a10848c231d8d7400323b7472524c6
Priya <priyanshumaurya8721@gmail.com>
2025-09-27 01:37:14 +0530

    Schema validation

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.post("/signup", async(req,res)=>{
         res.status(404).send("error will occured");
     }
 })
-
+// find the user by providing some data
 app.get("/user",async(req,res)=>{
     const userEmail = req.body.emailId;
     try{
@@ -31,11 +31,73 @@ app.get("/user",async(req,res)=>{
         res.status(404).send("Something went wrong");
     }
 })
-
+// find all the user 
 app.get("/feed",async(req,res)=>{
     try{
         const users = await User.find({}); // print the all users
         res.send(users);
+    }
+    catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+// find user by an id
+app.get("/id",async(req,res)=>{
+    try{
+        const id="68d45bd761f04d99bd8960c6"
+        const users = await User.findById(id);
+        res.send(users);
+    }
+    catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+// delete user from database
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("Data deleted sucessfully");
+    }
+    catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+// update the user data by the help of userId
+app.patch("/user/:userId",async(req,res)=>{
+    const userId = req.body.params?.userId;
+    const data = req.body;
+    try{
+        const Allowed_Updates = [
+            "gender","age","photoUrl","skills","about"
+        ];
+        const isUpdateAllowed = Object.keys(data).every((k)=>
+            Allowed_Updates.includes(k)
+        );
+        if(!isUpdateAllowed)
+        {
+            throw new Error("Update not allowed");
+        }
+        if(data?.skills.length > 10)
+        {
+            throw new Error("skills cannot more than 10");
+        }
+        const user = await User.findByIdAndUpdate(userId,data,{
+        runValidators:true,
+        });
+        res.send("update data sucessfully");
+    }
+    catch(err){
+        res.status(404).send("Update Failed" +err.message);
+    }
+})
+// update the user data by the help of emailId
+app.patch("/user1",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    const data = req.body;
+    try{
+        await User.findOneAndUpdate({emailId: userEmail},data);
+        res.send("update data sucessfully");
     }
     catch(err){
         res.status(404).send("Something went wrong");
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,57 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName: {
         type:String,
+        required:true,
+        minLength:4,
+        maxLength:50,
     },
     lastName: {
         type:String,
-    },
-    gender: {
-        type:String,
+        minLength:3,
+        maxLength:50,
     },
     emailId: {
         type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
+        trim:true,
     },
-    password: {
+     password: {
         type:String,
+        required:true,
+        minLength:9,
+        maxLength:50,
+    },
+     gender: {
+        type:String,
+        required:true,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
     },
     age: {
         type: Number,
+        min:18,
     },
-})
+    photoUrl: {
+        type: String,
+        default:"https://static.vecteezy.com/system/resources/previews/014/720/398/non_2x/man-user-icon-avatar-in-clipart-vector.jpg",
+    },
+    about: {
+        type:String,
+        maxLength:100,
+        default:"This is default about the user",
+    },
+    skills: {
+        type:[String],
+    }
+},
+{
+    timestamps:true,
+}
+)
 
 module.exports= mongoose.model("User",userSchema);
